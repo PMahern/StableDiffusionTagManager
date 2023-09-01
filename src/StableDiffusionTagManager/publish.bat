055a5527120d2f0606991bc606992dc08231b4c0
Paisley <paisley.mahern@gmail.com>
2023-08-31 20:19:58 -0500

    Added osx to the publish targets.

--- src/StableDiffusionTagManager/publish.bat
+++ src/StableDiffusionTagManager/publish.bat
@@ -1,2 +1,3 @@
-dotnet publish -c Release -r win10-x64 /p:PublishSingleFile=true
-dotnet publish -c Release -r linux-x64 /p:PublishSingleFile=true
+dotnet publish -c Release -r win10-x64 /p:PublishSingleFile=true --version-suffix beta1
+dotnet publish -c Release -r linux-x64 /p:PublishSingleFile=true --version-suffix beta1
+dotnet publish -c Release -r osx-x64 /p:PublishSingleFile=true --version-suffix beta1
