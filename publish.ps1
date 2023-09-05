31b67f4641fcf941cfae9ff48738dd925542cd62
Paisley <paisley.mahern@gmail.com>
2023-09-05 10:00:44 -0500

    Updated version in publish script.

--- publish.ps1
+++ publish.ps1
@@ -1,4 +1,4 @@
-$versionsuffix = "beta5"
+$versionsuffix = "beta6"
 $projectpath = "src/StableDiffusionTagManager/StableDiffusionTagManager.csproj"
 $platforms = "win10-x64", "linux-x64", "osx-x64"
 
