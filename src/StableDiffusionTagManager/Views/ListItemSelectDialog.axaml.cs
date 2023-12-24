b0bcc52445d5424883125e2697099bcbf467ae64
Paul Mahern <paisley.mahern@gmail.com>
2023-12-24 11:10:07 -0600

    Started work on list item select dialog.

---
+++ src/StableDiffusionTagManager/Views/ListItemSelectDialog.axaml.cs
@@ -0,0 +1,14 @@
+using Avalonia.Controls;
+
+namespace StableDiffusionTagManager.Views
+{
+    public partial class ListItemSelectDialog : Window
+    {
+        public ListItemSelectDialog()
+        {
+            InitializeComponent();
+        }
+
+
+    }
+}
