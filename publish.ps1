499f58fe624d23a1d359c627ea67279d69c69e6b
Paisley <paisley.mahern@gmail.com>
2023-12-27 21:30:19 -0600

    Updated publish version.

--- publish.ps1
+++ publish.ps1
@@ -1,4 +1,4 @@
-$versionsuffix = "beta8"
+$versionsuffix = "beta9"
 $projectpath = "src/StableDiffusionTagManager/StableDiffusionTagManager.csproj"
 $platforms = "win10-x64", "linux-x64", "osx-x64"
 
