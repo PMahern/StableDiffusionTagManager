d343e8c3301c1162300c6877e2d51182bc6cf9b2
Paul Mahern <paul.p.mahern@erdc.dren.mil>
2025-04-02 07:37:24 -0500

    Working on configuring selection aspect ratio presets.

--- src/StableDiffusionTagManager/Views/SettingsDialog.axaml.cs
+++ src/StableDiffusionTagManager/Views/SettingsDialog.axaml.cs
@@ -1,10 +1,31 @@
 using Avalonia;
 using Avalonia.Controls;
+using CommunityToolkit.Mvvm.ComponentModel;
 using CommunityToolkit.Mvvm.Input;
+using StableDiffusionTagManager.Extensions;
 using StableDiffusionTagManager.Models;
+using StableDiffusionTagManager.ViewModels;
+using System.Collections.ObjectModel;
+using System.Linq;
 
 namespace StableDiffusionTagManager.Views
 {
+    public partial class ImageResolution : ViewModelBase {
+        [ObservableProperty]
+        private int x;
+
+        [ObservableProperty] 
+        private int y;
+    }
+    public partial class ImageAspectRatioSet : ViewModelBase
+    {
+        [ObservableProperty]
+        private string name;
+
+        [ObservableProperty]
+        private ObservableCollection<ImageResolution> resolutions = new();
+    }
+
     public partial class SettingsDialog : Window
     {
         public SettingsDialog(Settings settings)
@@ -13,6 +34,11 @@ namespace StableDiffusionTagManager.Views
 
             this.WebUIAddress = settings.WebUiAddress;
             this.PythonPath = settings.PythonPath;
+            this.ImageAspectRatioSets = settings.ImageAspectRatioSets.Select(t => new ImageAspectRatioSet()
+            {
+                Name = t.Item1,
+                Resolutions = t.Item2.Select(r => new ImageResolution() { X = r.Item1, Y = r.Item2 }).ToObservableCollection()
+            }).ToObservableCollection();
             this.DataContext = this;
             this.settings = settings;
         }
@@ -42,11 +68,36 @@ namespace StableDiffusionTagManager.Views
             set => SetValue(PythonPathProperty, value);
         }
 
+        public static readonly StyledProperty<ObservableCollection<ImageAspectRatioSet>> ImageAspectRatioSetsProperty =
+            AvaloniaProperty.Register<SettingsDialog, ObservableCollection<ImageAspectRatioSet>>(nameof(ImageAspectRatioSets));
+
+        /// <summary>
+        /// Gets or sets if control can render the image
+        /// </summary>
+        public ObservableCollection<ImageAspectRatioSet> ImageAspectRatioSets
+        {
+            get => GetValue(ImageAspectRatioSetsProperty);
+            set => SetValue(ImageAspectRatioSetsProperty, value);
+        }
+
+        public static readonly StyledProperty<ImageAspectRatioSet?> SelectedImageAspectRatioSetProperty =
+            AvaloniaProperty.Register<SettingsDialog, ImageAspectRatioSet?>(nameof(SelectedImageAspectRatioSet));
+
+        /// <summary>
+        /// Gets or sets if control can render the image
+        /// </summary>
+        public ImageAspectRatioSet? SelectedImageAspectRatioSet
+        {
+            get => GetValue(SelectedImageAspectRatioSetProperty);
+            set => SetValue(SelectedImageAspectRatioSetProperty, value);
+        }
+
         [RelayCommand]
         public void Save()
         {
             settings.WebUiAddress = WebUIAddress;
             settings.PythonPath = PythonPath;
+            settings.ImageAspectRatioSets = ImageAspectRatioSets.Select(t => (t.Name, t.Resolutions.Select(r => (r.X, r.Y)).ToList())).ToList();
             settings.Save();
 
             Close();
@@ -62,6 +113,40 @@ namespace StableDiffusionTagManager.Views
         public void HeaderClose()
         {
             Cancel();
+        }
+
+        [RelayCommand]
+        public void DeleteResolution(ImageResolution resolution)
+        {
+            if (SelectedImageAspectRatioSet != null)
+            {
+                SelectedImageAspectRatioSet.Resolutions.Remove(resolution);
+            }
+        }
+
+        [RelayCommand]
+        public void AddResolution()
+        {
+            if(SelectedImageAspectRatioSet != null)
+            {
+                SelectedImageAspectRatioSet.Resolutions.Add(new ImageResolution());
+            }
+        }
+
+        [RelayCommand]
+        public void AddAspectRatioSet()
+        {
+            ImageAspectRatioSets.Add(new ImageAspectRatioSet());
+        }
+
+        [RelayCommand]
+        public void DeleteAspectRatioSet()
+        {
+            if (SelectedImageAspectRatioSet != null)
+            {
+                ImageAspectRatioSets.Remove(SelectedImageAspectRatioSet);
+                SelectedImageAspectRatioSet = null;
+            }
         }
     }
 }
