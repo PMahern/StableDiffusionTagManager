bf6df2cfa35cb87008030128544d17c51cf9de02
Paul Mahern <paul.p.mahern@erdc.dren.mil>
2025-04-06 11:26:58 -0500

    Updated version.

--- publish.ps1
+++ publish.ps1
@@ -1,4 +1,4 @@
-$versionsuffix = "0.20.0"
+$versionsuffix = "0.21.0-b1"
 $projectpath = "src/StableDiffusionTagManager/StableDiffusionTagManager.csproj"
 $platforms = "win10-x64", "linux-x64", "osx-x64"
 
